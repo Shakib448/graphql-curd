451db6912d6c0d898cfa5ab5828f876ed45d95ba
Shakiba448 <shakiba448@gmail.com>
2021-11-14 08:22:06 +0600

    WIP on graphql-curd

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,12 +3,18 @@ import { graphqlHTTP } from "express-graphql";
 import cors from "cors";
 import { createConnection } from "typeorm";
 
+const PORT = process.env.PORT || 5000;
+
 const main = async () => {
   const app = express();
   app.use(cors());
   app.use(express.json());
 
-  app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
+  //   app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
+
+  app.listen(PORT, () => {
+    console.log(`Server responding http://localhost:${PORT}`);
+  });
 };
 
 main().catch((error) => {
