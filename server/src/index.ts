b0cc72d450ea1e7addf0359cb3cc29356b0e2ae8
Shakiba448 <shakiba448@gmail.com>
2021-11-14 08:33:57 +0600

    WIP on graphql-curd

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import cors from "cors";
-import { createConnection } from "typeorm";
+import { createConnection, Connection } from "typeorm";
 
 const PORT = process.env.PORT || 5000;
 
 const main = async () => {
+  const connection: Connection = await createConnection({
+    type: "mongodb",
+    host: "localhost",
+    port: 27017,
+    database: "test",
+  });
   const app = express();
   app.use(cors());
   app.use(express.json());
