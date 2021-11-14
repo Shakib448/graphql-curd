0cb7d412dd3c292c32fa1a6dad70b9fb75e422f1
Shakiba448 <shakiba448@gmail.com>
2021-11-14 08:20:46 +0600

    WIP on graphql-curd

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,3 +2,15 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import cors from "cors";
 import { createConnection } from "typeorm";
+
+const main = async () => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+
+  app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
+};
+
+main().catch((error) => {
+  console.log(error);
+});
