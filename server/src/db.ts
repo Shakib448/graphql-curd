8237aa7c12c8a7ba21640fe06164c74ce59cf2a3
Shakiba448 <shakiba448@gmail.com>
2021-11-14 18:43:05 +0600

    WIP on graphql

--- server/src/db.ts
+++ server/src/db.ts
@@ -4,4 +4,5 @@ const dbURL = "mongodb://localhost:27017/GraphqlCURD";
 
 export async function connectDB(): Promise<void> {
   await connect(dbURL);
+  console.log("Database connected successfully");
 }
