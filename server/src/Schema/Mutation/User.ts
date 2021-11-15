123c7f37c97f0b7a16635e3e60e27aad6d9039ac
Shakiba448 <shakiba448@gmail.com>
2021-11-15 09:52:52 +0600

    WIP on graphql

--- server/src/Schema/Mutation/User.ts
+++ server/src/Schema/Mutation/User.ts
@@ -1,4 +1,4 @@
-import { GraphQLString } from "graphql";
+import { GraphQLString, GraphQLID } from "graphql";
 import { UserType } from "../TypeDefs/User";
 import User from "../Entities/Users";
 
@@ -11,5 +11,19 @@ export const CREATE_USER = {
   },
   async resolve(parent: any, args: any) {
     return await User.create(args);
+  },
+};
+
+export const DELETE_USER = {
+  type: UserType,
+  args: {
+    id: { type: GraphQLID },
+  },
+  async resolve(parent: any, args: any) {
+    const user = await User.findById(args.id);
+    if (user) {
+      const removeUser = await user.remove();
+      return removeUser;
+    }
   },
 };
--- server/src/Schema/index.ts
+++ server/src/Schema/index.ts
@@ -1,5 +1,5 @@
 import { GraphQLObjectType, GraphQLSchema } from "graphql";
-import { CREATE_USER } from "./Mutation/User";
+import { CREATE_USER, DELETE_USER } from "./Mutation/User";
 import { GET_ALL_USER } from "./Queries/User";
 
 const RootQuery = new GraphQLObjectType({
@@ -9,7 +9,7 @@ const RootQuery = new GraphQLObjectType({
 
 const Mutation = new GraphQLObjectType({
   name: "Mutation",
-  fields: { createUser: CREATE_USER },
+  fields: { createUser: CREATE_USER, deleteUser: DELETE_USER },
 });
 
 export const schema = new GraphQLSchema({
