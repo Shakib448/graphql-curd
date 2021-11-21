e4f7e1740147355b763bc2097d516e621d9c307d
Shakiba448 <shakiba448@gmail.com>
2021-11-21 18:35:23 +0600

    WIP on testing

--- server/src/__test__/queries.spec.ts
+++ server/src/__test__/queries.spec.ts
@@ -1,15 +1,31 @@
-import { query } from "express";
 import supertest from "supertest";
+import { connectDB } from "../config/db";
 import app from "../utils/server";
 
 const request = supertest(app);
 
 describe("Testing the queries", () => {
-  it("fetch users", async (done) => {
+  afterAll(async () => {
+    await connectDB();
+  });
+
+  it("fetch users", async () => {
     request
       .post("/graphql")
-      .send({ query: "{getAllUsers {name username}}" })
-      .expect(200);
-    done();
+      .send({
+        query: "{ getAllUsers{id, name, username, password} }",
+      })
+      .expect(200)
+      .end(function (err, res) {
+        res.body.data.getAllUsers.map((dt: any) => {
+          expect(typeof dt.id).toBe("string");
+          expect(typeof dt.name).toBe("string");
+          expect(typeof dt.username).toBe("string");
+          expect(typeof dt.password).toBe("string");
+        });
+        expect(res.body).toBeInstanceOf(Object);
+
+        expect(res.body.data.getAllUsers.length).toEqual(4);
+      });
   });
 });
